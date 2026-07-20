be0af6e9848ac116acc84aab786f216d1656be6c
sanskar <191781814+anotherhalf004@users.noreply.github.com>
2026-07-20 15:26:36 +0530

    React+JS - frontend , ui , state , hooks and api implemented in frontend . used axios , cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 // require auth routes
 import authRouter from './routes/auth.routes.js';
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials:true
+}))
 
 // using all routes
 app.use('/api/auth', authRouter);
